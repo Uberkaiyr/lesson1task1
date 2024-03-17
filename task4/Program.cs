1cd9541bbc366710a2fda0ff420acb84499467b5
Berkaiyr Ytepov <berkaiyr1996@mail.com>
2024-03-18 00:59:28 +0300

    add lesson1 task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 18;
+int b = 15;
+int c = 58;
+int d = 55;
+int e = 25;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
